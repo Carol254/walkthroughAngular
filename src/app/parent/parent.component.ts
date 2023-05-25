0769e19dafa841fa405be869d4f837686f977dc2
Caroline <wanguic49@gmail.com>
2023-05-25 11:29:58 +0300

    updating sharing info through dataService

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,13 +1,15 @@
 import { Component,ViewChild,AfterViewInit, OnInit } from '@angular/core';
 import { ChildComponent } from '../child/child.component';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit{
 
+
+export class ParentComponent implements OnInit{
 
   @ViewChild(ChildComponent) child:any;
 
@@ -15,28 +17,24 @@ export class ParentComponent implements OnInit{
 
   anotherMessageFromParent ="I am the second message";
 
-  
   message:string | undefined;
 
   receivedGreeting:string | undefined;
 
-  constructor(){}
+  newInfo:string | undefined;
 
+  constructor(private dataService:DataService){}
 
-  ngOnInit(): void {
-
-
-
+  ngOnInit() {
+    this.dataService.currentMessage.subscribe(message =>this.newInfo = message )
   }
 
   receiveMessage($event: string) {
     this.receivedGreeting = $event;
     }
 
-
   // ngAfterViewInit(){
   //   this.message = this.child.message;
   // }
 
- 
 }
--- src/app/sibling/sibling.component.ts
+++ src/app/sibling/sibling.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-sibling',
   templateUrl: './sibling.component.html',
   styleUrls: ['./sibling.component.css']
 })
-export class SiblingComponent {
+export class SiblingComponent implements OnInit{
 
+  message: string | undefined;
+
+  updatedMessage:string = 'Hello am from sibling';
+
+  constructor(private dataService:DataService){}
+
+  ngOnInit(){
+    this.dataService.currentMessage.subscribe(message =>this.message = message)
+  }
+
+  newMessage(){
+    this.dataService.changeMessage("Hello am from sibling");
+  }
 }
